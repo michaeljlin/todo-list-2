d278d0dcb71ada6806c03191f1883cbbc5057efa
michaeljlin <mjameslin@gmail.com>
2017-11-08 12:52:16 -0800

    fetching and rendering data from server

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 
 import App from './components/app';
+import promise from 'redux-promise';
 
 ReactDOM.render(
-    <Provider store={createStore(rootReducer, {}, applyMiddleware())}>
+    <Provider store={createStore(rootReducer, {}, applyMiddleware(promise))}>
         <App />
     </Provider>,
     document.getElementById('root')
--- src/reducers/todo_reducer.js
+++ src/reducers/todo_reducer.js
@@ -1,9 +1,12 @@
 import types from '../actions/types';
 
-const DEFAULT_STATE = {};
+const DEFAULT_STATE = { all:[] };
 
 export default function(state = DEFAULT_STATE, action){
     switch(action.type){
+        case types.GET_ALL:
+            console.log("GET ALL: ", action.payload);
+            return { all: action.payload.data.todos };
         default:
             return state;
     }
